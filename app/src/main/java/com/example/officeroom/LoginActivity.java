13ed256b5f851cb18dd3e7b4e167323b08462ab0
EvilMorty13 <aviksingha218@gmail.com>
2023-02-08 22:15:21 +0600

    Implemented login, changed text size from dp to sp

--- app/src/main/java/com/example/officeroom/LoginActivity.java
+++ app/src/main/java/com/example/officeroom/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.officeroom;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -7,16 +8,22 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
     TextView loginOfficeText;
     TextInputLayout loginMail,loginPassword;
     Button loginButton,registrationButton;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,9 +65,52 @@ public class LoginActivity extends AppCompatActivity {
                 },SPLASH_DURATION);
             }
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text_mail = loginMail.getEditText().getText().toString();
+                String text_password = loginPassword.getEditText().getText().toString();
+
+                boolean checked = checkConditions(text_mail,text_password);
+
+                if(checked) loginAccount(text_mail,text_password);
+
+            }
+        });
+    }
+
+    private void loginAccount(String text_mail, String text_password) {
+        auth.signInWithEmailAndPassword(text_mail,text_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean checkConditions(String text_mail, String text_password) {
+        if(TextUtils.isEmpty(text_mail)&&TextUtils.isEmpty(text_password)) {
+            Toast.makeText(this, "Enter Mail and Password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(TextUtils.isEmpty(text_mail)) {
+            Toast.makeText(this, "Enter Mail", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(TextUtils.isEmpty(text_password)) {
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
     private void findAllId() {
+        auth = FirebaseAuth.getInstance();
         loginMail = (TextInputLayout) findViewById(R.id.loginMailId);
         loginPassword = (TextInputLayout) findViewById(R.id.loginPasswordId);
         loginButton = (Button) findViewById(R.id.signInButtonId);
