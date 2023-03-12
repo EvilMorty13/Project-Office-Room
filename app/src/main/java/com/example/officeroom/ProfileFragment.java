1e4dcd33cb0387508596034daab3e69390d72088
EvilMorty13 <aviksingha218@gmail.com>
2023-03-12 22:22:53 +0600

    Changes in profile design, sign out dialog box color change

--- app/src/main/java/com/example/officeroom/ProfileFragment.java
+++ app/src/main/java/com/example/officeroom/ProfileFragment.java
@@ -43,7 +43,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        setStatusBarColor(getResources().getColor(R.color.white));
+        setStatusBarColor(getResources().getColor(R.color.backgroundColor));
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         findAllId(view);
 
